1297f9753bd15e45ffffda0718b491a7d0a2a878
Diego Simões <diegosean93@gmail.com>
2022-08-10 21:44:53 -0300

    adicionado bootstrap e json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <Header></Header>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react'
+
+export function Header(){
+    return (
+        <>
+        <h1>Vamos ver o que acontece</h1>
+        <ul className="list-group">
+            <li className="list-group-item">Teste 1</li>
+            <li className="list-group-item">Teste 2</li>
+            <li className="list-group-item">Teste 3</li>
+        </ul>
+        </>
+    )
+}
