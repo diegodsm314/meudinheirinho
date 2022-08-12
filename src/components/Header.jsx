6820290f8499d622b23dc3cf40273c182faeb066
marlenemoraes <marlenevmoraes@outlook.com>
2022-08-12 19:54:09 +0000

    add style in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,41 @@
 import { React, useEffect, useState } from 'react';
-import '../App.css'
+import '../App.css';
+import '../components/Header.css';
 import Button from 'react-bootstrap/Button';
 import user from '../services/user'
 import image from '../../public/vite.svg'
 
 
-export function Header(){
-    const [nomes,setNomes] = useState([]);
-    const [nome,setNome] = useState("ABC");
-    
+export function Header() {
+    const [nomes, setNomes] = useState([]);
+    const [nome, setNome] = useState("ABC");
 
-    useEffect(()=> {
+
+    useEffect(() => {
         //setar o usuario IMCOMPLETO
-        user.get("users").then(({data}) => {
+        user.get("users").then(({ data }) => {
             setNomes(data);
             //setNome(data.user);
         })
-    },[])
+    }, [])
 
     return (
-        <>
-        <img src={image} alt="logo" srcset="" />
-        <p className='headerIcon'>Meu dinheirinho</p>
-        <p className="name">Seja bem vindo {nome}</p>
-        <div className="mb-2">
-            <Button
-                variant="secondary"
-                size="lg"
+        <header>
+            <div className='logo'>
+                <img src={image} alt="logo" srcset="" />
+                <p className='headerIcon'>Meu dinheirinho</p>
+            </div>
+
+            <p className="name">Seja bem vindo {nome}</p>
+            <div className="mb-2">
+                <Button
+                    variant="secondary"
+                    size="lg"
+                    className='btn-transaction'
                 >
                     Nova transação
                 </Button>
-        </div>
-        </>
+            </div>
+        </header>
     )
 }
