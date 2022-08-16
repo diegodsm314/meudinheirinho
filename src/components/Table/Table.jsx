e390b04f0dc11dad0e4c867e4e69d0fade7ad867
Diego <diegosean93@gmail.com>
2022-08-16 22:04:08 +0000

    table construida

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 
-export function TableUnique(){
-    return(
-        <>
-        </>
+export function TableUnique() {
+    const [tab,setTab] = useState([]);
+
+    useEffect(() => {
+        //setar o usuario IMCOMPLETO
+        user.get("count").then(({ data }) => {
+            setTab(data);
+        })
+    }, [])
+    
+
+    return (
+        <div className="table">
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{"A"}</td>
+                        <td>{"B"}</td>
+                        <td>{"C"}</td>
+                        <td>{"D"}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
     )
 }
