07f90000f54abbda3962a9824ded4b5326d8fd24
marlenemoraes <marlenevmoraes@outlook.com>
2022-08-18 18:08:39 +0000

    update imports on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main';
 import { Card } from 'react-bootstrap';
-import { Login } from './components/Login/Login';
 import GlobalProvider from './context/GlobalProvider';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   const handleCss = () => {
