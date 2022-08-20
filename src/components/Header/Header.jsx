4936f002b75a4139736a76890265e083a6dd89ac
marlenemoraes <marlenevmoraes@outlook.com>
2022-08-20 21:01:31 +0000

    responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,54 @@
 import { React, useEffect, useState, useContext } from 'react';
-import '../../App.css';
-import './Header.css';
-import image from '../../../public/vite.svg'
+import { Button, Container, Form } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
 import GlobalContext from "../../context/GlobalContext";
+import image from '../../../public/vite.svg';
 import { Login } from '../Login/Login';
 import { ModalUnique } from '../Expense/Modal';
+import '../../App.css';
+import './Header.css';
 
 
 export function Header() {
     const globalCtx = useContext(GlobalContext);
     const [nome, setNome] = useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         setNome(globalCtx.login)
-    },[globalCtx])
+    }, [globalCtx])
 
     return (
         <header>
-            <div className='logo'>
-                <img src={image} alt="logo"/>
-                <p className='headerIcon'>Meu dinheirinho</p>
-            </div>
-            <div className="login">
-                <p className="name">Seja bem vindo {nome}</p>
-            </div>
-            <div className="buttons mb-2">
-                <Login></Login>
-                <ModalUnique></ModalUnique>
-            </div>
+            <Navbar key='lg' expand='lg' id='navbar' variant="dark">
+                <Container fluid>
+                    <Navbar.Brand className='logo' href="#">
+                        <img src={image} alt="logo" />
+                        <p className='headerIcon'>Meu dinheirinho</p>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
+                    <Navbar.Offcanvas
+                        id='offcanvas'
+                        aria-labelledby='offcanvasNavbarLabel-expand-lg'
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id='offcanvasNavbarLabel-expand-lg' className='logo'>
+                                <img src={image} alt="logo" />
+                                <p className='headerIcon'>Meu dinheirinho</p>
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-center flex-grow-1 pe-3 login">
+                                <p className="name">Seja bem vindo {nome}</p>
+                            </Nav>
+                            <div className="buttons mb-2">
+                                <Login></Login>
+                                <ModalUnique></ModalUnique>
+                            </div>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
         </header>
     )
 }
